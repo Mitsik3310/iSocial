7b4fe7d6d3621aa549cdc47d8f65050c386d1102
Mitsik3310 <98687651+Mitsik3310@users.noreply.github.com>
2022-04-28 19:25:47 +0200

    register, but it doesn't work

---
+++ pages/registration.php
@@ -0,0 +1,110 @@
+<?php
+require_once('config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+    <head>
+        <meta charset="UTF-8" />
+        <title>I social - Rejestracja</title>
+        <meta name="description" content="Najpopularniejszy portal społecznościowy. Załóż konto i stań się członkiem już dziś." />
+        <meta name="keywords" content="iSocial, isocial, social, register" />
+        <link rel="shortcut icon" href="../media/logo_rounded.png" />
+        <link rel="stylesheet" href="../styles/register_styles.css" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
+
+        <script src="../scripts/typing.js" async></script>
+        <script src="../scripts/valregister.js" async></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="title">Zarejestruj się!</div>
+            <form name="form" action="registration.php" method="post">
+                <div class="user_info">
+                    <div class="input">
+                        <label for="firstname" class="text_above_fill">Imię</label>
+                        <input type="text" placeholder="Podaj swoje imię." id="firstname" name="firstname" required />
+                    </div>
+                    <div class="input">
+                        <label for="lastname" class="text_above_fill">Nazwisko.</label>
+                        <input type="text" placeholder="Podaj swoje nazwisko." id="lastname" name="lastname" required />
+                    </div>
+                    <div class="input">
+                        <label for="email" class="text_above_fill">Email</label>
+                        <input type="email" placeholder="Podaj swój email." id="email" name="email" required />
+                    </div>
+                    <div class="input">
+                        <label for="phonenumber" class="text_above_fill">Numer telefonu</label>
+                        <input type="text" placeholder="Podaj swój numer telefonu." id="phonenumber" name="phonenumber" required />
+                    </div>
+                    <div class="input">
+                        <label for="password" class="text_above_fill">Hasło</label>
+                        <input type="password" placeholder="Podaj swoje hasło." id="password" name="password" required />
+                    </div>
+                    <div class="input">
+                        <span class="text_above_fill">Potwierdź hasło *</span>
+                        <input type="password" placeholder="Powtórz swoje hasło." required name="text4" id="passrep" />
+                    </div>
+                </div>
+                <div class="submit">
+                  <input type="submit" name="create" class="button" id="register" value="Zarejestruj się"/>
+                </div>
+                <div class="acc">Masz już konto? <a href="login.html">Zaloguj się teraz.</a></div>
+            </form>
+        </div>
+
+        <div class="text">
+            <p>iSocial jest:&nbsp;</p>
+            <span class="word midnight">bezpieczny.</span>
+            <span class="word wisteria">nowoczesny.</span>
+            <span class="word belize">darmowy.</span>
+            <span class="word green">inteligentny.</span>
+            <span class="word wisteria">stylowy.</span>
+            <span class="word pomegranate">niezwykły.</span>
+        </div>
+    </body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+    <script type="text/javascript">
+        $(function () {
+            $("#register").click(function (e) {
+                var valid = this.form.checkValidity();
+
+                if (valid) {
+                    var firstname = $("#firstname").val();
+                    var lastname = $("#lastname").val();
+                    var email = $("#email").val();
+                    var phonenumber = $("#phonenumber").val();
+                    var password = $("#password").val();
+
+                    e.preventDefault();
+
+                    $.ajax({
+                        type: "POST",
+                        url: "process.php",
+                        data: { firstname: firstname, lastname: lastname, email: email, phonenumber: phonenumber, password: password },
+                        success: function (data) {
+                            Swal.fire({
+                                title: "Operacja zakończona sukcesem",
+                                text: data,
+                                type: "success",
+                            });
+                        },
+                        error: function (data) {
+                            Swal.fire({
+                                title: "Błąd",
+                                text: "Wystąpił błąd przy zapisywaniu danych. Spróbuj ponownie później.",
+                                type: "error",
+                            });
+                        },
+                    });
+                } else {
+                }
+            });
+        });
+    </script>
+</html>
