f33c044578f2044bd0500ef7835acf99882fda4b
Mitsik3310 <98687651+Mitsik3310@users.noreply.github.com>
2022-05-22 22:48:28 +0200

    eoo

---
+++ pages/users.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 20 Maj 2022, 19:36
+-- Wersja serwera: 10.4.22-MariaDB
+-- Wersja PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phonenumber` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `user_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `user_id`, `firstname`, `lastname`, `email`, `phonenumber`, `password`, `user_name`) VALUES
+(24, '1184815', 'Mateusz', 'Fidut', 'mitsikfizdut@gmail.com', '', 'Test1234', 'Test1234'),
+(25, '925578508', 'Barnaba', 'Barnaba', 'Barnaba2@gmail.com', '', 'Barnaba1', 'Barnaba');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- scripts/alert.js
+++ scripts/alert.js
@@ -1,5 +1,5 @@
 function showAlert() {
     var info = "Wciśnięcie przycisku 'ok' rozumiane jest jako znajomość i pełna akceptacja warunków użytkowania usługi, jak i polityki prywatności naszej firmy";
     alert (info);
-    setTimeout("location.href='https://mitsik3310.github.io/iSocial/';",1);
+    setTimeout("location.href='index.php';",1);
   }
